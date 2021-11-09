ef14f622f6a2d52cbcb0134ae60d8e73890bb00e
tnotenson <tomas.notenson@gmail.com>
2021-11-09 14:55:52 -0300

    Varying hz

--- Kicked_Ising_evolution.py
+++ Kicked_Ising_evolution.py
@@ -52,14 +52,14 @@ def fU(N, J, hx, hz):
     
     return U
 
-def Evolution_KI_Tinf(time_lim, N, J, hx, hz, A, B):
+def Evolution4p_KI_Tinf(time_lim, N, J, hx, hz, A, B):
     
     start_time = time.time() 
     
     # define arrays for data storage
-    OTOCs = np.zeros((time_lim))#[]
-    O1s = np.zeros((time_lim))#[]
-    O2s = np.zeros((time_lim))#[]
+    OTOCs = np.zeros((time_lim), dtype=np.complex_)#[]
+    O1s = np.zeros((time_lim), dtype=np.complex_)#[]
+    O2s = np.zeros((time_lim), dtype=np.complex_)#[]
         
     # define floquet operator
     U = fU(N, J, hx, hz)
@@ -88,13 +88,50 @@ def Evolution_KI_Tinf(time_lim, N, J, hx, hz, A, B):
 
         
     print(f"\nTOTAL --- {time.time() - start_time} seconds ---" )
-    flag = 'KI_with_Tinf_state'
+    flag = '4p_KI_with_Tinf_state'
     return [OTOCs, O1s, O2s, flag]
+
+def Evolution2p_KI_Tinf(time_lim, N, J, hx, hz, A, B):
+    
+    start_time = time.time() 
+    
+    # define arrays for data storage
+    Cs = np.zeros((time_lim), dtype=np.complex_)#[]
+        
+    # define floquet operator
+    U = fU(N, J, hx, hz)
+    
+    # compute OTOC, O1 and O2 for each time
+    for i in tqdm(range(time_lim), desc='Evolution loop'):
+        
+        if i==0:
+            B_t = B
+        else:
+            # Evolution
+             B_t = B_t.transform(U.dag())# U*B_t*U.dag()
+        
+        # compute 2-point correlator
+        dim = 2**N
+        C_t = (B_t*A).tr()
+        C_t = C_t/dim
+
+        
+        # store data
+        Cs[i] = np.abs(C_t)
+
+        
+    print(f"\nTOTAL --- {time.time() - start_time} seconds ---" )
+    flag = '2p_KI_with_Tinf_state'
+    return [Cs, flag]
 #%%
-N = 8
-J = 1*np.ones(N)
+N = 10
+J = 0.7*np.ones(N)
 hx = 0.5*np.ones(N)
-hz = 0.5*np.ones(N)
+hz = 1.1*np.ones(N)
+
+j = np.mean(J)
+x = np.mean(hx)
+z = np.mean(hz)
 
 sx_list = []
 sy_list = []
@@ -117,8 +154,42 @@ B = sum(sz_list)
 
 time_lim = 30
 
-OTOCs, O1s, O2s, flag = Evolution_KI_Tinf(time_lim, N, J, hx, hz, A, B)
-#%% plot results
+OTOCs, O1s, O2s, flag = Evolution4p_KI_Tinf(time_lim, N, J, hx, hz, A, B)
+# Cs, flag = Evolution2p_KI_Tinf(time_lim, N, J, hx, hz, A, B)
+#%% plot 2-point correlator results
+
+# times = np.arange(0,time_lim)
+
+# # define colormap
+# # colors = plt.cm.jet(np.linspace(0,1,len(Ks)))
+
+# # create the figure
+# plt.figure(figsize=(12,8), dpi=100)
+
+# # all plots in the same figure
+# plt.title(f'2-point correlator N = {N} A = Z, B = Z')
+    
+# # compute the Ehrenfest's time
+# # tE = np.log(N)/ly1[i]
+    
+# # plot log10(C(t))
+
+
+# yaxis = np.log10(Cs)
+# plt.plot(times, yaxis,':^r' , label=r'$T = \infty$');
+
+# # plot vertical lines in the corresponding Ehrenfest's time for each K
+# # plt.vlines(tE, np.min(np.log10(OTOC_Ks[0]*factor)+b), np.max(np.log10(OTOC_Ks[0]*factor)+b), lw=0.5, ls='dashed', alpha=0.8, color=colors[i])
+    
+# plt.xlabel('Time');
+# plt.ylabel(r'$log \left(C(t) \right)$');
+# # plt.xlim((0,10))
+# # plt.ylim((-12,0))
+# plt.grid();
+# plt.legend(loc='best');
+# plt.show()
+# plt.savefig('2pC_'+flag+f'_J{j}_hz{x}_hz{z}_basis_size{N}_AZ_BZ.png', dpi=300)#_Gauss_sigma{sigma}
+#%% plot OTOC, O1, O2 results
 
 times = np.arange(0,time_lim)
 
@@ -138,7 +209,7 @@ plt.title(f'OTOC N = {N} A = Z, B = Z')
 
 
 yaxis = np.log10(OTOCs)
-plt.plot(times, yaxis,':^r' , label=r'T = \infty');
+plt.plot(times, yaxis,':^r' , label=r'$T = \infty$');
 
 # plot vertical lines in the corresponding Ehrenfest's time for each K
 # plt.vlines(tE, np.min(np.log10(OTOC_Ks[0]*factor)+b), np.max(np.log10(OTOC_Ks[0]*factor)+b), lw=0.5, ls='dashed', alpha=0.8, color=colors[i])
@@ -150,7 +221,7 @@ plt.ylabel(r'$log \left(C(t) \right)$');
 plt.grid();
 plt.legend(loc='best');
 plt.show()
-plt.savefig('OTOC_'+flag+f'_J{np.mean(J)}_basis_size{N}_AZ_BZ.png', dpi=300)#_Gauss_sigma{sigma}
+plt.savefig('OTOC_'+flag+f'_J{j}_hz{x}_hz{z}_basis_size{N}_AZ_BZ.png', dpi=300)#_Gauss_sigma{sigma}
 
 # create the figure
 plt.figure(figsize=(12,8), dpi=100)
@@ -165,7 +236,7 @@ plt.title(f'O1 N = {N} A = Z, B = Z')
 
 
 yaxis = np.log10(O1s)
-plt.plot(times, yaxis,':^r' , label=r'T = \infty');
+plt.plot(times, yaxis,':^r' , label=r'$T = \infty$');
 
 # plot vertical lines in the corresponding Ehrenfest's time for each K
 # plt.vlines(tE, np.min(np.log10(OTOC_Ks[0]*factor)+b), np.max(np.log10(OTOC_Ks[0]*factor)+b), lw=0.5, ls='dashed', alpha=0.8, color=colors[i])
@@ -177,7 +248,7 @@ plt.ylabel(r'$log \left(O_1(t) \right)$');
 plt.grid();
 plt.legend(loc='best');
 plt.show()
-plt.savefig('O1_'+flag+f'_J{np.mean(J)}_basis_size{N}_AZ_BZ.png', dpi=300)#_Gauss_sigma{sigma}
+plt.savefig('O1_'+flag+f'_J{j}_hz{x}_hz{z}_basis_size{N}_AZ_BZ.png', dpi=300)#_Gauss_sigma{sigma}
 
 # create the figure
 plt.figure(figsize=(12,8), dpi=100)
@@ -192,7 +263,7 @@ plt.title(f'O2 N = {N} A = Z, B = Z')
 
 
 yaxis = np.log10(O2s)
-plt.plot(times, yaxis,':^r' , label=r'T = \infty');
+plt.plot(times, yaxis,':^r' , label=r'$T = \infty$');
 
 # plot vertical lines in the corresponding Ehrenfest's time for each K
 # plt.vlines(tE, np.min(np.log10(OTOC_Ks[0]*factor)+b), np.max(np.log10(OTOC_Ks[0]*factor)+b), lw=0.5, ls='dashed', alpha=0.8, color=colors[i])
@@ -204,4 +275,4 @@ plt.ylabel(r'$log \left(O_2(t) \right)$');
 plt.grid();
 plt.legend(loc='best');
 plt.show()
-plt.savefig('O2_'+flag+f'_J{np.mean(J)}_basis_size{N}_AZ_BZ.png', dpi=300)#_Gauss_sigma{sigma}
+plt.savefig('O2_'+flag+f'_J{j}_hz{x}_hz{z}_basis_size{N}_AZ_BZ.png', dpi=300)#_Gauss_sigma{sigma}
