f8ee4db9e58b85c66adb4467f5b5bd3ae1704f1f
tnotenson <tomas.notenson@gmail.com>
2021-11-12 11:33:43 -0300

    pa' la ofi

--- XXZ.py
+++ XXZ.py
@@ -132,14 +132,15 @@ def r_chaometer(ener,normed=False):
     return ra
 
 # reduce to a subspace with Sz = M and parity = 1
-def chbasis_bloque(H, ind_1, pari=1):
+def chbasis_bloque(A, B, H, ind_1, pari=1):
     # define number of qubits
     N = len(H.dims[0])
     # define parity operator
     P = parity(N)
     
     # reduce H and parity to Sz = M subspace
-    
+    A_red = A.extract_states(ind_1)
+    B_red = B.extract_states(ind_1)
     P_red = P.extract_states(ind_1)
     H_red = H.extract_states(ind_1)
     
@@ -163,26 +164,44 @@ def chbasis_bloque(H, ind_1, pari=1):
     # construct the base change matrix
     C = [aut_red[i].data.todense() for i in range(len(aut_red))]
     C = np.array(C, ndmin=2)[:,:,0]
+    
     # change from computational to energy basis
+    A_sub = A_red.transform(C)
+    B_sub = B_red.transform(C)
     H_sub = H_red.transform(C)
     P_sub = P_red.transform(C)
     
     # reduce to a subspace with Sz = M and parity = 1
+    A_ssub = A_sub.extract_states(ind_par)
+    B_ssub = B_sub.extract_states(ind_par)
     H_ssub = H_sub.extract_states(ind_par)
     P_ssub = P_sub.extract_states(ind_par)
-               
+    
+    print(A_ssub.shape)
+    
+    C = Qobj(C)
+    # C.dims = A.dims
+    C_ssub = C.extract_states(ind_par)
+    
+    print(C_ssub.shape)
+    
     # check
     
     print('\n',ind_par) # energy basis index with parity 1
     print('\n',H_ssub) # check that H is diagonal
     print('\n',P_ssub) # # check that parity is equal (prop) to identity
     
-    return H_ssub, ind_par
+    A_res = A_ssub.transform(C_ssub.inv())
+    B_res = B_ssub.transform(C_ssub.inv())
+    H_res = H_ssub.transform(C_ssub.inv())
+    
+    
+    return H_res, A_res, B_res
 
 
 #%%
 # define H's parameters
-N = 12
+N = 8
 J = 1
 mu = 0.5
 lamb = 0.5
@@ -191,19 +210,6 @@ lamb = 0.5
 exc = int(N/4)
 H = XXZ(N, J, mu, lamb)
 
-# search index of computational basis with Sz = M
-time_0 = time.time()
-ind_1=sz_subspace_diag(N,exc)
-time_1 = time.time()
-print(f'sz_subspace_diag :{time_1 - time_0} seg')
-
-# reduce to a subspace with Sz = M and parity = 1
-H_sub, ind_par = chbasis_bloque(H, ind_1)
-time_2 = time.time()
-print(f'\nchbasis_bloque :{time_2 - time_1} seg')
-# print(f'\nIs H_sub diagonal?: {not bool(np.count_nonzero(H_sub - np.diag(np.diagonal(H_sub))))}')
-# print(f'H_sub diagonal: {H_sub.diag()}')
-#%%
 sx_list = []
 sy_list = []
 sz_list = []
@@ -221,17 +227,34 @@ for n in range(N):
 
     op_list[n] = sz
     sz_list.append(tensor(op_list))
-    
 
-def Evolution2p_KI_Tinf(H, time_lim, N, J, hx, hz, A, B):
+A = sz_list[int(N/2)]
+B = sz_list[int(N/2)]
+
+# search index of computational basis with Sz = M
+time_0 = time.time()
+ind_1=sz_subspace_diag(N,exc)
+time_1 = time.time()
+print(f'sz_subspace_diag :{time_1 - time_0} seg')
+
+# reduce to a subspace with Sz = M and parity = 1
+H_sub, A_res, B_res = chbasis_bloque(A, B, H, ind_1)
+time_2 = time.time()
+print(f'\nchbasis_bloque :{time_2 - time_1} seg')
+# print(f'\nIs H_sub diagonal?: {not bool(np.count_nonzero(H_sub - np.diag(np.diagonal(H_sub))))}')
+# print(f'H_sub diagonal: {H_sub.diag()}')
+#%%
+def Evolution2p_KI_Tinf(H, time_lim, N, A, B):
     
     start_time = time.time() 
     
     # define arrays for data storage
-    Cs = np.zeros((time_lim), dtype=np.complex_)#[]
+    Cs = np.zeros((time_lim))#, dtype=np.complex_)#[]
         
     # define time evolution operator
     U = H.expm()
+    
+    # print(U)
     
     # compute OTOC, O1 and O2 for each time
     for i in tqdm(range(time_lim), desc='Evolution loop'):
@@ -240,14 +263,14 @@ def Evolution2p_KI_Tinf(H, time_lim, N, J, hx, hz, A, B):
             B_t = B
         else:
             # Evolution
-             B_t = B_t.transform(U.dag())# U*B_t*U.dag()
+            B_t = B_t.transform(U.dag())# U*B_t*U.dag()
         
         # compute 2-point correlator
         dim = 2**N
         C_t = (B_t*A).tr()
         C_t = C_t/dim
 
-        
+        print(C_t)
         # store data
         Cs[i] = np.abs(C_t)
 
@@ -256,6 +279,44 @@ def Evolution2p_KI_Tinf(H, time_lim, N, J, hx, hz, A, B):
     flag = '2p_KI_with_Tinf_state'
     return [Cs, flag]
 
+
+time_lim = 30
+
+Cs, flag = Evolution2p_KI_Tinf(H_sub, time_lim, N, A_res, B_res)
+
+#%% plot 2-point correlator results
+
+times = np.arange(0,time_lim)
+
+# # define colormap
+# # colors = plt.cm.jet(np.linspace(0,1,len(Ks)))
+
+# # create the figure
+plt.figure(figsize=(12,8), dpi=100)
+
+# # all plots in the same figure
+plt.title(f'2-point correlator N = {N}')
+    
+# # compute the Ehrenfest's time
+# # tE = np.log(N)/ly1[i]
+    
+# plot log10(C(t))
+
+
+yaxis = np.log10(Cs)
+plt.plot(times, yaxis,':^r' , label=r'$T = \infty$');
+
+# # plot vertical lines in the corresponding Ehrenfest's time for each K
+# # plt.vlines(tE, np.min(np.log10(OTOC_Ks[0]*factor)+b), np.max(np.log10(OTOC_Ks[0]*factor)+b), lw=0.5, ls='dashed', alpha=0.8, color=colors[i])
+    
+plt.xlabel('Time');
+plt.ylabel(r'$log \left(C(t) \right)$');
+# plt.xlim((0,10))
+# plt.ylim((0,1e5))
+plt.grid();
+plt.legend(loc='best');
+plt.show()
+# plt.savefig('2pC_'+flag+f'_J{j}_hz{x}_hz{z}_basis_size{N}_AZ_BZ.png', dpi=300)#_Gauss_sigma{sigma}
 
 
     
