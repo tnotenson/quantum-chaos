c6dd1c6e52c5fd962b7b6704dd88d5a7154d3cda
tnotenson <tomas.notenson@gmail.com>
2022-03-08 13:53:15 -0300

    Corregidos los plots

--- TFIM_2pC.py
+++ TFIM_2pC.py
@@ -153,6 +153,18 @@ def Sj(N, j='z'):
             s_list.append(tensor(op_list))
         return sum(s_list)
 
+def sigmai_j(N,i,j='z'):
+
+    op_list = [si for m in range(N)]
+
+    if j == 'z':
+        op_list[i] = sz
+    elif j == 'x':
+        op_list[i] = sx
+    elif j == 'y':
+        op_list[i] = sy
+    return tensor(op_list)
+
 def can_bas(N,i):
     e = np.zeros(N)
     e[i] = 1.0
@@ -201,6 +213,13 @@ def histo_level_spacing(ener):
     plt.xlabel('level spacing')
     return 
 
+# @jit(nopython=True)#, parallel=True, fastmath = True)
+def diagH_r(H_sub):
+    ener = np.linalg.eigvals(H_sub)
+    ener = np.sort(ener)
+    histo_level_spacing(ener)
+    r_normed = r_chaometer(ener, plotadjusted=True) 
+    return r_normed
 
 # @jit(nopython=True)#, parallel=True, fastmath = True)
 def diagU_r(U_sub):
@@ -271,19 +290,8 @@ def diagU_r(U_sub):
 #
 #U_sub = U_parity(dim, U, par_basis_ones)
 #%%
-def Evolution2p_H_KI_Tinf(H, time_lim, N, A, B):
-    
-    start_time = time() 
-    
-    # define arrays for data storage
-    Cs = np.zeros((time_lim))#, dtype=np.complex_)#[]
-        
-    # define time evolution operator
-    U = (-1j*H).expm().data.toarray()
-    U = np.matrix(U, dtype=np.complex_)
-    Udag = U.H
-    # print(U)
-    
+
+def C2p_time(time_lim, A, B, U, Udag, Cs, N):
     # compute OTOC, O1 and O2 for each time
     for i in tqdm(range(time_lim), desc='Evolution loop'):
         
@@ -302,8 +310,24 @@ def Evolution2p_H_KI_Tinf(H, time_lim, N, A, B):
         C_t = twopC_numpy_Tinf(A, B_t)# - A_average(A, dim)
         C_t = C_t/dim/N
         
-        print(C_t)
+        # print(C_t)
+        Cs[i] = np.abs(C_t)
+    return Cs
 
+def Evolution2p_H_KI_Tinf(H, time_lim, N, A, B):
+    
+    start_time = time() 
+    
+    # define arrays for data storage
+    Cs = np.zeros((time_lim))#, dtype=np.complex_)#[]
+        
+    # define time evolution operator
+    U = (-1j*H).expm().data.toarray()
+    U = np.matrix(U, dtype=np.complex_)
+    Udag = U.H
+    # print(U)
+    
+    Cs = C2p_time(time_lim, A, B, U, Udag, Cs, N)
         
     print(f"\nTOTAL --- {time() - start_time} seconds ---" )
     flag = '2p_H_KI_with_Tinf_state'
@@ -320,34 +344,8 @@ def Evolution2p_U_KI_Tinf(U, time_lim, N, A, B):
 #    U = fU(N, J, hx, hz, theta)
     Udag = U.H
 
-
-    # compute OTOC, O1 and O2 for each time
-    for i in tqdm(range(time_lim), desc='Evolution loop'):
-        
-        if i==0:
-            B_t = B
-        else:
-
-            # qutip evolution
-            # B_t = B_t.transform(U.dag())
-            # numpy evolution
-            B_t = Evolucion_numpy(B_t, U, Udag)
-            
-        dim = A.shape[0]
-        # compute 2-point correlator with qutip
-        # C_t = (B_t*A).tr() - A.tr()/dim
-        # C_t = C_t/dim
-        
-        # compute 2-point correlator with qutip
-        C_t = twopC_numpy_Tinf(A, B_t)# - A_average(A, dim)
-        C_t = C_t/dim/N
-        
-        # print(C_t)
-        
-        # store data
-        Cs[i] = np.abs(C_t)
-
-        
+    Cs = C2p_time(time_lim, A, B, U, Udag, Cs, N)
+    
     print(f"\nTOTAL --- {time() - start_time} seconds ---" )
     flag = '2p_U_KI_with_Tinf_state'
     return [Cs, flag]
@@ -363,11 +361,14 @@ def TFIM_2pC_chaos_parameter(N, B, J, theta, time_lim):
     
     start_time = time()
     # let's try it
-    
+    # ######## H evolution ##########
+    # H = TFIM(N, hx, hz, Jz)
     ######## U evolution ##########
     U = fU(N, Jz, hx, hz)
     
     A = Sj(N, j='x')#/N
+    # i = int(N/2)
+    # A = sigmai_j(N,i,j='x')
     
     # op = 'X'
     
@@ -386,13 +387,23 @@ def TFIM_2pC_chaos_parameter(N, B, J, theta, time_lim):
     print('tamaños de subespacios de par.', n_mone, n_one)
     C = np.column_stack([vec.data.toarray() for vec in epvec])
     Cinv = np.linalg.inv(C)
-        
+    
+    # ######## H evolution ##########
+    
+    # H_par = H.transform(Cinv)
+    # # print(H_par)
+    
     ######## U evolution ##########
     U_par = U.transform(Cinv)
     # print(U_par)
     
     A_par = A.transform(Cinv)
-
+    
+    
+    # ######## H evolution ##########
+    # H_sub = H_par.extract_states(np.arange(n_mone,n_one+n_mone))
+    # # print(H_sub)
+    
     ######## U evolution ##########
     U_sub = U_par.extract_states(np.arange(n_mone, n_mone+n_one)).data.toarray()
     U_sub = np.matrix(U_sub)
@@ -400,10 +411,14 @@ def TFIM_2pC_chaos_parameter(N, B, J, theta, time_lim):
     A_sub = A_par.extract_states(np.arange(n_mone, n_mone+n_one)).data.toarray()
     print(f"\n Separate parity eigenstates --- {time() - start_time} seconds ---" )
     
+    # r_normed_H = diagH_r(H_sub)
     r_normed_U = diagU_r(U_sub)
     
     start_time = time()
     #
+      
+    # Cs, flag = Evolution2p_H_KI_Tinf(H_sub, time_lim, N, A_sub, A_sub)
+    # np.savez(flag+f'_time_lim{time_lim}_J{J:.2f}_hx{x:.2f}_hz{z:.2f}_theta{theta:.2f}_basis_size{N}'+operators+'.npz', O1s=O1s_H, O2s=O2s_H, Cs=Cs_H)
     
     Cs, flag = Evolution2p_U_KI_Tinf(U_sub, time_lim, N, A_sub, A_sub)
     # np.savez('2pC_'+flag+f'_time_lim{time_lim}_J{J:.2f}_hx{x:.2f}_hz{z:.2f}_basis_size{N}'+operators+'.npz', Cs=Cs)
@@ -460,21 +475,28 @@ if hx == hz:
     plt.ylim(-4,0.2)
 else:
     y_C2_U = Cs
-    plt.plot(times, np.mean(y_C2_U)*np.ones(time_lim), '-.k', lw=2, label=r'$\langle C \rangle = $'+f'{np.mean(y_C2_U).real:.2f}')
     
-plt.plot(times,y_C2_U, 'o:k', ms=1, lw=2, label='$C_2(t)$', alpha=0.8)
+    if hz == 0:
+        DL = 0.485
+    elif hz == 0.4:
+        DL = 0.293
+    
+    plt.plot(times, np.mean(y_C2_U)*np.ones(time_lim), '-.k', lw=2, label=r'$D/L = $'+f'{DL:.3f}')
+    
+plt.plot(times,y_C2_U, 'o:k', ms=1, lw=2, label='$L=$'+f'{N}', alpha=0.8)
 # plt.plot(xs, poly1d_fn_p(xs), '--r', lw=1)
 
 
 
 
-plt.xlabel(r'$\theta$')
+plt.xlabel(r'$t$')
 plt.ylabel(r'$log_{10}(C(t))$')
 plt.xlim(-0.2,50.2)
 plt.grid()
 plt.legend(loc='best')
-opA = 'X'
-opB = 'X'
+# opi = str(int(N/2))
+opA = 'X'#+opi
+opB = opA#'X'
 paridad = 'par'
 operators = '_A'+opA+'_B'+opB
 BC = 'PBC'
